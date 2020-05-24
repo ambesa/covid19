d68d66854ffa2afa1168a92d1cdd5301f80d5485
anbesajer pawlos <ambesa4@gmail.co>
2020-05-24 14:57:06 -0700

    Updates

---
+++ precache-manifest.ba6c3b8a76f9faec3f979b25b21947d2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "add6c899e679b4864f55760d6c2daef5",
+    "url": "/ambesa/covid19.git/index.html"
+  },
+  {
+    "revision": "b79ddfc4d4791601ad0b",
+    "url": "/ambesa/covid19.git/static/css/main.3cb6394e.chunk.css"
+  },
+  {
+    "revision": "8291c53b464917af8e89",
+    "url": "/ambesa/covid19.git/static/js/2.ff3a8a7c.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/ambesa/covid19.git/static/js/2.ff3a8a7c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b79ddfc4d4791601ad0b",
+    "url": "/ambesa/covid19.git/static/js/main.4e8ee177.chunk.js"
+  },
+  {
+    "revision": "11e99ef7793d086a1eb2",
+    "url": "/ambesa/covid19.git/static/js/runtime-main.f3f11d44.js"
+  }
+]);
